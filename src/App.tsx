8241ba5e407ffe7d17ef007e6a23cea377140445
Zachary Bailey <zacharyb2504@gmail.com>
2026-09-06 17:26:15 -0400

    Added my name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Name: Zachary Bailey
             </p>
         </div>
     );
