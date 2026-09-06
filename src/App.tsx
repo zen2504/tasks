84e146760d7821821d8c0afa514361f6ebcf25fd
Zachary Bailey <zacharyb2504@gmail.com>
2026-09-06 18:06:36 -0400

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Name: Zachary Bailey
+                automatically reload. Name: Zachary Bailey &quot;Hello
+                World&quot;
             </p>
         </div>
     );
